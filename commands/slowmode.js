23dd1ea01f0bc11d07cda155d5534412dc74b0ac
Infinitywastaken <Dervishidaniel987@gmail.com>
2020-09-25 16:12:05 +0200

    Slowmode Finished
    Slowmode Command!

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -1,5 +1,11 @@
 const { Channel } = require("discord.js")
 
 exports.run = function(bot,message,args){
-    message.channel.setRateLimitPerUser(5);
+    if (!message.member.hasPermission(["MANAGE_CHANNELS"])) return message.channel.send("You can't use this command")
+    var time = message.content.split(' ').slice(2).join(' ')
+    if (!time) return message.channel.send('Please state a time the slowmode can be')
+    message.channel.setRateLimitPerUser(time);
+    message.channel.send(`Set the slowmode to : **${time}** seconds`).then(m => m.delete({
+        timeout: 2000
+    }))
 }
