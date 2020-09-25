55287ed8669b73fc62ed83d84983b407b4f716b0
Infinitywastaken <Dervishidaniel987@gmail.com>
2020-09-25 16:18:57 +0200

    Avatar finished
    Avatar command!

---
+++ commands/avatar.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require("discord.js");
+exports.run = function(bot,message,args){
+    let Embed = new MessageEmbed();
+    let roles = [];
+    if (!message.mentions.users.first()) {
+      message.member.roles.cache.forEach((role) => {
+        roles.push(role.name);
+      });
+      Embed.setTitle(`Your avatar!`);
+      Embed.setThumbnail(message.author.displayAvatarURL());
+      Embed.setColor(`RANDOM`);
+      return message.channel.send(Embed);
+    } else {
+      let User = message.mentions.members.first();
+      User.roles.cache.forEach((role) => {
+        roles.push(role.name);
+      });
+      Embed.setTitle(`${bot.users.cache.get(User.id).tag}'s avatar!`);
+      Embed.setThumbnail(bot.users.cache.get(User.id).displayAvatarURL());
+      Embed.setColor(`RANDOM`);
+      return message.channel.send(Embed);
+    }
+  };
