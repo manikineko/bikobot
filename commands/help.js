cb6f26f6a9d8e95ba4b07015609907ad748ae88e
Bloom Sirenix <66263341+bloomsirenix@users.noreply.github.com>
2021-08-09 16:12:51 +0200

    fixed prefix in help command

--- commands/help.js
+++ commands/help.js
@@ -18,19 +18,19 @@ module.exports.help = {
     .setAuthor(client.user.username, client.user.avatarURL)
     .setDescription('Here some commands for you to use')
     .addFields(
-      { name: 'm>avatar [member]', value: ' shows the avatar of someone or you in the guild', inline: true },
-      { name: 'm>ban [member]', value: 'ban an guild member', inline: true },
-      { name: 'm>say [message]', value: 'Say something using the bot(requires manage massages permission)', inline: true },
-      { name: 'm>ping', value: 'pong', inline: true },
-      { name: 'm>slowmode [number]', value: 'set the slowmode for the channel(0 for none)', inline: true },
-      { name: 'm>activity', value: 'play an voice chat activity', inline: true },
-      { name: 'm>test', value: 'hello world!', inline: true },
-      { name: 'm>punnish', value: 'Currendly disabled', inline: true }, //m>punnish | punnish a user(Requires aditional permissions to use)
-      { name: 'm>help', value: 'opens this', inline: true },
-      { name: 'm>getmessage [messageid]', value: ' gets a message id(must be internal id)', inline: true },
-      { name: 'm>mkchannel [name] [position]', value: 'make channel with a name at a certian position(might be excluded to default to bottom)', inline: true },
-      { name: 'm>mkthread [name] ', value: 'make thread with a name', inline: true },
-      { name: 'm>rawsay ', value: 'blocked dev command', inline: true },
+      { name: 'b>avatar [member]', value: ' shows the avatar of someone or you in the guild', inline: true },
+      { name: 'b>ban [member]', value: 'ban an guild member', inline: true },
+      { name: 'b>say [message]', value: 'Say something using the bot(requires manage massages permission)', inline: true },
+      { name: 'b>ping', value: 'pong', inline: true },
+      { name: 'b>slowmode [number]', value: 'set the slowmode for the channel(0 for none)', inline: true },
+      { name: 'b>activity', value: 'play an voice chat activity', inline: true },
+      { name: 'b>test', value: 'hello world!', inline: true },
+      { name: 'b>punnish', value: 'Currendly disabled', inline: true }, //b>punnish | punnish a user(Requires aditional permissions to use)
+      { name: 'b>help', value: 'opens this', inline: true },
+      { name: 'b>getmessage [messageid]', value: ' gets a message id(must be internal id)', inline: true },
+      { name: 'b>mkchannel [name] [position]', value: 'make channel with a name at a certian position(might be excluded to default to bottom)', inline: true },
+      { name: 'b>mkthread [name] ', value: 'make thread with a name', inline: true },
+      { name: 'b>rawsay ', value: 'blocked dev command', inline: true },
     )
     .addField('Embed and bot is Copyright © Bloom Sirenix#6090 made for', client.user.username+" running on " + client.guilds.cache.size + " servers with " + client.users.cache.size + " users in total", true)
     .setImage(client.user.avatarURL)
