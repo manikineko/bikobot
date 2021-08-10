851f3f8b98b2d22d27698f4913803d02abe3c8aa
Roman Beard <62947856+Roman-Beard@users.noreply.github.com>
2021-08-10 13:24:47 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -47,3 +47,155 @@ module.exports.help = {
       name:"help"
     }
     */
+
+
+
+
+
+
+
+const Discord = require("discord.js");
+const { disbut, MessageActionRow, MessageButton } = require("discord-buttons");
+
+module.exports.help = {
+    name:"help"
+  },
+  run: async (client, message, args) => {
+    const embed = new Discord.MessageEmbed()
+      .setDescription(
+        `**__Biko's Help Menu__**\n\nemoji here - Utility\nemoji here - Fun\nemoji here - Misc\nemoji here - Moderation\nemoji here - Activities`
+      )
+      .setFooter(
+        "React with one of the following emojis to see the avaliable commands for that category!",
+        message.author.displayAvatarURL()
+      )
+      .setTimestamp()
+      .setThumbnail(client.user.displayAvatarURL());
+
+    const embed1 = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setDescription(
+        `emoji here __Utility Commands__ emoji here\n\n• Pu your list of commands here for this category.`
+      )
+      .setFooter(
+        `Command Requested by: ${message.author.tag}`,
+        message.author.displayAvatarURL()
+      );
+
+    const embed2 = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setDescription(
+        `emoji here __Fun Commands__ emoji here\n\n• Put your list of commands here for this category.`
+      )
+      .setFooter(
+        `Command Requested by: ${message.author.tag}`,
+        message.author.displayAvatarURL()
+      );
+
+    const embed3 = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setDescription(
+        `emoji here __Misc Commands__ emoji here\n\n• Put your list of commands here for this category.`
+      )
+      .setFooter(
+        `Command Requested by: ${message.author.tag}`,
+        message.author.displayAvatarURL()
+      );
+
+    const embed4 = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setDescription(
+        `emoji here __Moderation Commands__ emoji here\n\n• Put your list of commands here for this category.`
+      )
+      .setFooter(
+        `Command Requested by: ${message.author.tag}`,
+        message.author.displayAvatarURL()
+      );
+
+    const embed5 = new Discord.MessageEmbed()
+      .setColor("RANDOM")
+      .setDescription(
+        `emoji here __Activity Commands__ emoji here\n\n• Put your list of commands here for this category.`
+      )
+      .setFooter(
+        `Command Requested by: ${message.author.tag}`,
+        message.author.displayAvatarURL()
+      );
+    let button1 = new MessageButton()
+      .setLabel(``)
+      .setID(`help1`)
+      .setEmoji(`emoji goes here`)
+      .setStyle("blurple");
+
+    let button2 = new MessageButton()
+      .setLabel(``)
+      .setID(`help2`)
+      .setEmoji(`emoji goes here`)
+      .setStyle("blurple");
+
+    let button3 = new MessageButton()
+      .setLabel(``)
+      .setID(`help3`)
+      .setEmoji(`emoji goes here`)
+      .setStyle("blurple");
+
+    let button4 = new MessageButton()
+      .setLabel(``)
+      .setID(`help4`)
+      .setEmoji(`emoji goes here`)
+      .setStyle("blurple");
+    //if (!message.member.hasPermission("MANAGE_MESSAGES"))
+    //button4.setDisabled(true);
+
+    let button5 = new MessageButton()
+      .setLabel(``)
+      .setID(`help5`)
+      .setEmoji(`emoji goes here`)
+      .setStyle("blurple");
+
+    let row = new MessageActionRow().addComponents(
+      button1,
+      button2,
+      button3,
+      button4,
+      button5
+    );
+
+    const MESSAGE = await message.channel.send(embed, row);
+
+    const filter = button => button.clicker.user.id === message.author.id;
+    //const collector = MESSAGE.createButtonCollector(filter, { time: 120000 });
+    const collector = MESSAGE.createButtonCollector(filter);
+
+    collector.on("collect", async b => {
+      if (b.id == "help1") {
+        MESSAGE.edit(embed1, row);
+        await b.reply.defer();
+      }
+
+      if (b.id == "help2") {
+        MESSAGE.edit(embed2, row);
+        await b.reply.defer();
+      }
+
+      if (b.id == "help3") {
+        MESSAGE.edit(embed3, row);
+        await b.reply.defer();
+      }
+
+      if (b.id == "help4") {
+        MESSAGE.edit(embed4, row);
+        await b.reply.defer();
+      }
+
+      if (b.id == "help5") {
+        MESSAGE.edit(embed5, row);
+        await b.reply.defer();
+      }
+    });
+  }
+};
+
+module.exports.help = {
+      name:"help"
+    }
